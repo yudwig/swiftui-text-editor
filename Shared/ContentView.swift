bf17e9b9264c3e5f16fc9137ef7d23cf1614f0bf
yudwig <yudwig.dev@gmail.com>
2021-07-01 23:42:11 +0900

    Initial commit.

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  
+//  
+//  Created by yudwig on 2021/07/01
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swiftui_text_editorApp.swift
@@ -0,0 +1,18 @@
+//
+//  swiftui_text_editorApp.swift
+//  
+//  
+//  Created by yudwig on 2021/07/01
+//  
+//
+
+import SwiftUI
+
+@main
+struct swiftui_text_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
