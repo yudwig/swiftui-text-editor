90f0da81710f6458a53fe4137c633ca7a7dd3062
yudwig <yudwig.dev@gmail.com>
2021-07-02 22:10:38 +0900

    Add font color & size pickers.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,9 +11,16 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var text: String = "place holder."
+    @State private var color: Color
+    @State private var fontSize = 15.0
+    @State private var isEditingFontSize: Bool = false
+    
+    init() {
+        color = Color(red: 0, green: 0, blue: 1.0)
+    }
     
     var body: some View {
-        HStack(content: {
+        VStack(content: {
             setting
             editor
         })
@@ -21,16 +28,34 @@ struct ContentView: View {
     }
     
     var setting: some View {
-        return Text("settings")
+        VStack(content: {
+            Text("settings").multilineTextAlignment(.leading)
+            ColorPicker("Foreground color", selection: $color)
+            VStack(content: {
+                Text("Font size")
+                Slider(
+                    value: $fontSize,
+                    in: 0...50,
+                    step: 5,
+                    onEditingChanged: { editing in
+                        isEditingFontSize = editing
+                    }
+               )
+            })
+        })
     }
     
     var editor: some View {
         return TextEditor(text: $text)
+            .foregroundColor(color)
+            .font(.custom("HelveticaNeue", size: CGFloat(fontSize)))
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        ContentView.init()
         ContentView()
     }
 }
